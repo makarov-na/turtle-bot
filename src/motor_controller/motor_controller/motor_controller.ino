543b5820d2eaed802b991aca443dcd59f8869224
Nikolay Makarov <makarov-na@yandex.ru>
2025-07-27 20:06:29 +0300

    Rotation test

--- src/motor_controller/motor_controller/motor_controller.ino
+++ src/motor_controller/motor_controller/motor_controller.ino
@@ -3,6 +3,11 @@
 #define IN1_R 2   // Направление 1
 #define IN2_R 3   // Направление 2
 
+// Определение пинов для левого мотора
+#define ENB_L 6   // ШИМ-контроль скорости
+#define IN3_L 4   // Направление 1
+#define IN4_L 7   // Направление 2
+
 // Константы скорости
 const int MIN_SPEED = 60;   // Минимальная рабочая скорость (0-255)
 const int MAX_SPEED = 255;  // Максимальная скорость
@@ -12,62 +17,90 @@ const int STEP_DELAY = ACCEL_TIME / ACCEL_STEPS; // Задержка между 
 
 void setup() {
   // Настройка пинов управления как выходы
+  // Правый мотор
   pinMode(ENA_R, OUTPUT);
   pinMode(IN1_R, OUTPUT);
   pinMode(IN2_R, OUTPUT);
   
-  // Изначальная остановка мотора
-  brakeMotor();
+  // Левый мотор
+  pinMode(ENB_L, OUTPUT);
+  pinMode(IN3_L, OUTPUT);
+  pinMode(IN4_L, OUTPUT);
+  
+  // Изначальная остановка моторов
+  brakeMotors();
 }
 
 void loop() {
-  // Вперед с разгоном
-  setMotorForward();
-  accelerateMotor(MIN_SPEED, MAX_SPEED);
+  // Движение вперед с разгоном
+  setRightMotorForward();
+  setLeftMotorBackward();
+  accelerateMotors(MIN_SPEED, MAX_SPEED);
   
   // Остановка на 3 секунды
-  brakeMotor();
+  brakeMotors();
   delay(3000);
   
-  // Назад с разгоном
-  setMotorBackward();
-  accelerateMotor(MIN_SPEED, MAX_SPEED);
+  // Движение назад с разгоном
+  setRightMotorBackward();
+  setLeftMotorForward();
+  accelerateMotors(MIN_SPEED, MAX_SPEED);
   
   // Финальная остановка
-  brakeMotor();
+  brakeMotors();
   
   // Остановка программы
   while(true);
 }
 
-// Установить направление "Вперед"
-void setMotorForward() {
-  digitalWrite(IN1_R, HIGH);
-  digitalWrite(IN2_R, LOW);
-}
-
-// Установить направление "Назад"
-void setMotorBackward() {
+// Установить направление "Вперед" для правого мотора
+void setRightMotorForward() {
   digitalWrite(IN1_R, LOW);
   digitalWrite(IN2_R, HIGH);
 }
 
-// Функция плавного разгона/торможения
-void accelerateMotor(int startSpeed, int endSpeed) {
+// Установить направление "Назад" для правого мотора
+void setRightMotorBackward() {
+  digitalWrite(IN1_R, HIGH);
+  digitalWrite(IN2_R, LOW);
+}
+
+// Установить направление "Вперед" для левого мотора
+void setLeftMotorForward() {
+  digitalWrite(IN3_L, LOW);
+  digitalWrite(IN4_L, HIGH);
+}
+
+// Установить направление "Назад" для левого мотора
+void setLeftMotorBackward() {
+  digitalWrite(IN3_L, HIGH);
+  digitalWrite(IN4_L, LOW);
+}
+
+// Функция плавного разгона для обоих моторов
+void accelerateMotors(int startSpeed, int endSpeed) {
   float speedIncrement = (endSpeed - startSpeed) / (float)ACCEL_STEPS;
   float currentSpeed = startSpeed;
   
   for(int i = 0; i < ACCEL_STEPS; i++) {
     analogWrite(ENA_R, (int)currentSpeed);
+    analogWrite(ENB_L, (int)currentSpeed);
     currentSpeed += speedIncrement;
     delay(STEP_DELAY);
   }
-  analogWrite(ENA_R, endSpeed); // Гарантия достижения максимума
+  analogWrite(ENA_R, endSpeed);
+  analogWrite(ENB_L, endSpeed);
 }
 
-// Функция остановки мотора
-void brakeMotor() {
+// Функция остановки обоих моторов
+void brakeMotors() {
+  // Правый мотор
   analogWrite(ENA_R, 0);
   digitalWrite(IN1_R, LOW);
   digitalWrite(IN2_R, LOW);
+  
+  // Левый мотор
+  analogWrite(ENB_L, 0);
+  digitalWrite(IN3_L, LOW);
+  digitalWrite(IN4_L, LOW);
 }
